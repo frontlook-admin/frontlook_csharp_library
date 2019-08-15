951c470c6d1a5ccd0cf69168bd66690095b9a286
Debanjan Chakraborty <frontlook.inc@gmail.com>
2019-08-16 01:31:43 +0530

    library section data_helper added

---
+++ frontlook_csharp_library/dbf_helper.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Data.OleDb;
+using System.Data;
+
+namespace frontlook_csharp_library
+{
+    public static class dbf_helper
+    {
+        public static void get_os(string operatingSystem)
+        {
+            OperatingSystem os = Environment.OSVersion;
+            Version vs = os.Version;
+            if (os.Platform == PlatformID.Win32Windows)
+            {
+                //This is a pre-NT version of Windows
+                switch (vs.Minor)
+                {
+                    case 0:
+                        operatingSystem = "95";
+                        break;
+                    case 10:
+                        if (vs.Revision.ToString() == "2222A")
+                            operatingSystem = "98SE";
+                        else
+                            operatingSystem = "98";
+                        break;
+                    case 90:
+                        operatingSystem = "Me";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (os.Platform == PlatformID.Win32NT)
+            {
+                switch (vs.Major)
+                {
+                    case 3:
+                        operatingSystem = "NT 3.51";
+                        break;
+                    case 4:
+                        operatingSystem = "NT 4.0";
+                        break;
+                    case 5:
+                        if (vs.Minor == 0)
+                            operatingSystem = "2000";
+                        else
+                            operatingSystem = "XP";
+                        break;
+                    case 6:
+                        if (vs.Minor == 0)
+                            operatingSystem = "Vista";
+                        else if (vs.Minor == 1)
+                            operatingSystem = "7";
+                        else if (vs.Minor == 2)
+                            operatingSystem = "8";
+                        else
+                            operatingSystem = "8.1";
+                        break;
+                    case 10:
+                        operatingSystem = "10";
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public static void dbf_con_string(string dbf_directory_filepath, string dbf_constring1)
+        {
+            string operatingSystem = "";
+
+            string dbf_filename = "";
+
+            get_os(operatingSystem);
+            if (operatingSystem != "")
+            {
+                operatingSystem = "Windows " + operatingSystem;
+            }
+
+            switch (operatingSystem)
+            {
+                case "Windows XP":
+                    dbf_constring1 = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source = " + @dbf_directory_filepath + ";Extended Properties=dBase IV;User ID=;Password=";
+                    break;
+                case "Windows 7":
+                    dbf_constring1 = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source = " + @dbf_directory_filepath + ";Extended Properties=dBase IV;User ID=;Password=";
+                    break;
+                case "Windows Vista":
+                    dbf_constring1 = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source = " + @dbf_directory_filepath + ";Extended Properties=dBase IV;User ID=;Password=";
+                    break;
+                case "Windows 8":
+                    dbf_constring1 = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + @dbf_directory_filepath + ";Extended Properties=dBase IV;User ID=;Password=";
+                    break;
+                case "Windows 8.1":
+                    dbf_constring1 = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + @dbf_directory_filepath + ";Extended Properties=dBase IV;User ID=;Password=";
+                    break;
+                case "Windows 10":
+                    dbf_constring1 = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + @dbf_directory_filepath + ";Extended Properties=dBase IV;User ID=;Password=";
+                    break;
+                default:
+                    dbf_constring1 = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + @dbf_directory_filepath + ";Extended Properties=dBase IV;User ID=;Password=";
+                    break;
+            }
+        }
+
+        public static void getDataTable(string dbf_filepath, DataTable dt)
+        {
+            FileInfo fileInfo = new FileInfo(dbf_filepath);
+            string dbf_directory_filepath = fileInfo.DirectoryName;
+            string x = Path.GetDirectoryName(dbf_filepath);
+            string dbf_constring1 = "";
+            //Get version information about the os.
+            dbf_con_string(dbf_directory_filepath, dbf_constring1);
+
+            //Variable to hold our return value
+
+            string excelFilename = ""; string xml = ""; string xml_schema = ""; string s_without_ext = "";
+            string s = "";
+
+
+            s = dbf_filepath;
+            excelFilename = "";
+            s_without_ext = "";
+            excelFilename = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\" + s + ".xlsx";
+            s_without_ext = Path.GetFileNameWithoutExtension(s);
+
+            try
+            {
+
+                OleDbConnection connection = new OleDbConnection(dbf_constring1);
+                string sql = "SELECT * FROM " + s_without_ext;
+
+                OleDbCommand cmd1 = new OleDbCommand(sql, connection);
+                connection.Open();
+                OleDbDataAdapter DA = new OleDbDataAdapter(cmd1);
+                DA.Fill(dt);
+                //DA.Update(dt);
+                connection.Close();
+                //BackgroundWorker bgw = new BackgroundWorker();
+
+
+            }
+            catch (OleDbException e)
+            {
+                MessageBox.Show("Error : " + e.Message);
+            }
+        }
+    }
+}
